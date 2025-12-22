b9025c0951a293793d409d46cd40b40ccab862ae
qhdlflrla <qhddlfrla@gmail.com>
2025-12-22 14:00:00 +0900

    fix: 줄바꿈 정규식 개선 - 요/다/죠/음/됨/함/임/까 뒤 마침표 처리

--- scripts/script-generator.js
+++ scripts/script-generator.js
@@ -1439,13 +1439,16 @@ if (generateBlogBtn) {
                 }
             }
 
-            // ★ 마침표 후 줄바꿈 처리 (가독성 향상) ★
-            // 마침표+공백 뒤에 한글이 오면 줄바꿈 추가
-            blogContent = blogContent.replace(/\. ([가-힣])/g, '.\n\n$1');
-            // 물음표+공백 뒤에 한글이 오면 줄바꿈 추가
-            blogContent = blogContent.replace(/\? ([가-힣])/g, '?\n\n$1');
-            // 느낌표+공백 뒤에 한글이 오면 줄바꿈 추가
-            blogContent = blogContent.replace(/! ([가-힣])/g, '!\n\n$1');
+            // ★ 마침표/물음표/느낌표 후 줄바꿈 처리 (가독성 향상) ★
+            // 문장 끝 부호 다음에 줄바꿈 추가 (한글 다음 마침표, 공백 있든 없든)
+            // "요." / "다." / "죠." 등 다음에 줄바꿈
+            blogContent = blogContent.replace(/([요다죠음됨함임까])\.\s*/g, '$1.\n\n');
+            // 물음표 다음에 줄바꿈
+            blogContent = blogContent.replace(/\?\s*/g, '?\n\n');
+            // 느낌표 다음에 줄바꿈
+            blogContent = blogContent.replace(/!\s*/g, '!\n\n');
+            // 연속된 줄바꿈 정리 (3개 이상은 2개로)
+            blogContent = blogContent.replace(/\n{3,}/g, '\n\n');
 
             // 결과 표시 (이미지 프롬프트 제외)
             resultDiv.innerText = blogContent;
